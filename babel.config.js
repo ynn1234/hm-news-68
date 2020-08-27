ddcc9b7e517a8ee457d4978d68eb0d70a4034674
袁宁宁 <yuanning186@163.com>
2020-08-27 21:25:08 +0800

    添加基础样式,并引入vant

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,13 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
+
+  // vant按需引入的配置
+  // plugins: [
+  //   ['import', {
+  //     libraryName: 'vant',
+  //     libraryDirectory: 'es',
+  //     style: true
+  //   }, 'vant']
+  // ]
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 引入样式
+import './styles/bass.less'
+import './styles/iconfont.css'
 import router from './router'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
